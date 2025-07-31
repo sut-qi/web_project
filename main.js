0b1c7c38747074f4a288910924aa6901c0b30d92
cyber-torpedo <torpedo.cyber@gmail.com>
2025-07-31 19:24:55 +0800

    big change to jokes

--- main.js
+++ main.js
@@ -2,25 +2,10 @@ import { readFile } from "fs/promises";
 import { createServer } from "http";
 
 const server = createServer(async (req, res) => {
-  //   HTML Example
   if (req.url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     const htmlFile = await readFile("./hello.html", "utf-8");
     res.end(htmlFile);
-  }
-
-  //   JSON Example
-  if (req.url === "/info") {
-    res.writeHead(200, { "content-type": "application/json" });
-    const jsonFile = await readFile("./data.json", "utf-8");
-    res.end(jsonFile);
-  }
-
-  //   Text Example
-  if (req.url === "/text") {
-    res.writeHead(200, { "content-type": "text/plain" });
-    const textFile = await readFile("./hello.txt", "utf-8");
-    res.end(textFile);
   }
 
   if (req.url === "/mc.png") {
@@ -28,6 +13,7 @@ const server = createServer(async (req, res) => {
     res.writeHead(200, { "content-type": "image/png" });
     res.end(img);
   }
+
   if (req.url === "/hello.js") {
     const jsFile = await readFile("./hello.js", "utf-8");
     res.writeHead(200, { "content-type": "text/javascript" });
