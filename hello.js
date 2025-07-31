121a7fff175b97a1c45b231c91478f6ed18e2057
cyber-torpedo <torpedo.cyber@gmail.com>
2025-07-31 22:48:19 +0800

    big change joke successful

---
+++ hello.js
@@ -0,0 +1,22 @@
+const joke = document.querySelector(".joke");
+const button = document.querySelector("button");
+
+async function fetchJoke() {
+  try {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    const result = data.joke;
+    joke.textContent = result;
+  } catch (error) {
+    console.error("Error fetching joke:", error);
+    joke.textContent = "Failed to fetch joke.";
+  }
+}
+button.addEventListener("click", fetchJoke);
+
+document.addEventListener("DOMContentLoaded", fetchJoke());
